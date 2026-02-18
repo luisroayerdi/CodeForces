58e23a30b007b2ea15ebbe1e61df1d568a78e205
Luis Ayerdi-Morales <luisroayerdi@hotmail.com>
2026-02-18 13:24:02 -0600

    bit++.cpp solution

---
+++ bit++.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+    int n;
+    cin >> n;
+
+    string x;
+
+    int count = 0;
+
+    for(int i = 0; i < n; i++){
+        cin >> x;
+
+        if (x == "++X" or "X++"){
+            count++;
+        }
+        else {
+            count--;
+        }
+    }
+
+    cout << count;
+}
