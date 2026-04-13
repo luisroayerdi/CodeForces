9d810fccc5749e3aeed27d0adcc6a13f71a7c4a1
Luis Ayerdi-Morales <luisroayerdi@hotmail.com>
2026-04-13 17:31:58 -0500

    Update team.cpp

--- team.cpp
+++ team.cpp
@@ -1,35 +1,34 @@
 #include <iostream>
 #include <vector>
-
 using namespace std;
 
-int main(){
-    int columns = 3;
+int main() {
+  int columns = 3;
 
-    int rows;
-    cin >> rows;
+  int rows;
+  cin >> rows;
 
-    vector<vector<int>> vec(rows, vector<int>(columns));
+  vector<vector<int>> vec(rows, vector<int>(columns));
 
-    for (int i = 0; i <rows; i++){
-        for (int j = 0; j<columns; j++){
-            cin >> vec[i][j];
-        }   
+  for (int i = 0; i < rows; i++) {
+    for (int j = 0; j < columns; j++) {
+      cin >> vec[i][j];
     }
+  }
 
-    int count = 0;
+  int count = 0;
 
-    for (int i = 0; i <rows; i++){
-        int count2 = 0;
-        for (int j = 0; j<columns; j++){
-            if (vec[i][j] == 1){
-                count2 += 1;
-            }
-        }
-        if (count2 >= 2){
-            count += 1;
-        }
+  for (int i = 0; i < rows; i++) {
+    int count2 = 0;
+    for (int j = 0; j < columns; j++) {
+      if (vec[i][j] == 1) {
+        count2 += 1;
+      }
     }
+    if (count2 >= 2) {
+      count += 1;
+    }
+  }
 
-    cout << count;
+  cout << count;
 }
