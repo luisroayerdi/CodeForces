db7d54f18046fa4abc7b911f4f084e7be6e93ff8
Luis Ayerdi-Morales <luisroayerdi@hotmail.com>
2026-04-13 17:31:58 -0500

    Update bit++.cpp

--- bit++.cpp
+++ bit++.cpp
@@ -1,25 +1,17 @@
+#include <cmath>
 #include <iostream>
 
 using namespace std;
 
-int main(){
-
-    int n;
-    cin >> n;
-    int count = 0;
-
-    for(int i = 0; i < n; i++){
-        string x;
-        cin >> x;
-
-        if (x == "++X" or x == "X++"){
-            count+=1;
-        }
-        else if (x == "--X" or x == "X--"){
-            count-=1;
-        }
-    }
-
-    cout << count;
-
+int main() {
+  int n;
+  cin >> n;
+  int count = 0;
+  string x;
+  cin >> x;
+  if (x == "++X" or x == "X++") {
+    count += 1;
+  } else if (x == "--X" or x == "X--") {
+    count -= 1;
+  }
 }
